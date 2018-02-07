58a9b7ada9621505a0d0c97e49686e2c6197cfb9
VGunkooo <v.gunkooo@gmail.com>
2018-02-07 19:03:24 +0200

    1

--- src/src/Hello.java
+++ src/src/Hello.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hello {
+
     public static void main(String[] args) {
-        System.out.println("hello 2");
+        List<String> list1 = new ArrayList<String>();
+        list1.add("hello");
+        list1.add(" Vova");
+        list1.add(" in GitHub");
+
+        for (String s : list1) {
+            System.out.print(s);
+        }
+
     }
+
 }
